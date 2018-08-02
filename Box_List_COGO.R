2c6775fc00294089850bcd2e594116b1d8110093
hdvince <41804055+hdvince@users.noreply.github.com>
2018-08-01 23:47:38 -0800

    Add files via upload

--- Box_List_COGO.R
+++ Box_List_COGO.R
@@ -47,3 +47,64 @@ check[order(check$since,decreasing=T), ]
   
  return(check[order(c(check$to.hatch),decreasing=F), ]) 
 }
+
+
+data=WhichBox("D:/incomplete_for_test.csv", today=149)
+data
+
+
+#considering COME incubation periods
+WhichBox=function(data.path, today=0){
+  
+  
+  
+  data=read.csv(data.path, header=T, stringsAsFactors = F)
+  data<-data[ ,1:13]
+  
+  if(today==0){
+    
+    warning("Today not provided. Using last recorded date.")
+    
+    today=max(as.numeric(data$date), na.rm=T)}
+  
+  box.list=unique(data$box)
+  box.list=box.list[!is.na(box.list)]
+  since=c()
+  to.pip=c()
+  to.hatch=c()
+  data$pip=(28-data$stage)+data$date
+  if [data$sp1=="COME"]{data$pip=(30-data$stage)+data$date}
+  data$hatch=(32-data$stage)+data$date
+  if [data$sp1=="COME"]{data$hatch=(32-data$stage)+data$date}
+  
+  
+  for(a in 1:length(box.list)){
+    
+    since[a]=today-max(as.numeric(data$date[data$box==box.list[a]]), na.rm=T)
+    
+    last.obs=data[data$box==box.list[a],]
+    last.obs=last.obs[which.max(last.obs$date),]
+    
+    to.pip[a]=last.obs$pip-today
+    to.hatch[a]=last.obs$hatch-today
+    
+  }  
+  
+  
+  
+  check<-data.frame(box=box.list, since=since, to.pip=to.pip, to.hatch=to.hatch)  
+  
+  
+  check[order(check$since,decreasing=T), ]
+  
+  
+  return(check[order(c(check$to.hatch),decreasing=F), ]) 
+}
+
+
+
+
+data=WhichBox("D:/incomplete_for_test.csv", today=149)
+data=WhichBox("/Volumes/USB20FD/R7MBM_COGO/incomplete_for_test.csv", today=149)
+
+data
